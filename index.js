b6f6de3ba722cf95b94412cad985316c826a4576
alekseygurenkov <aleksey.sknt@gmail.com>
2025-02-22 10:29:09 +0300

    filled in index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const sergey = new Person('sergey', 20);
+const pavel = new Person('pavel', 30);
+
+pavel.sayHi();
--- person.js
+++ person.js
@@ -9,7 +9,3 @@ class Person {
     }
 }
 
-const sergey = new Person('sergey', 20);
-const pavel = new Person('pavel', 30);
-
-pavel.sayHi();
